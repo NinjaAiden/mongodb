99d3a89db7aa171e176a7aa5bf021da00011ee40
Aiden Harvey <aidenharvey102@gmail.com>
2019-02-10 14:12:19 +0000

    added function to search for ebtries in database

--- mongo_project.py
+++ mongo_project.py
@@ -23,6 +23,21 @@ def show_menu():
     option = input("Enter an option: ")
     return option
 
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    
+    try:
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+    except:
+        print ("Error accessing databse")
+    if not doc:
+        print("")
+        print("Error! No results found")
+    
+    return doc
+
 def add_record():
     print("")
     first = input("Enter first name > ")
