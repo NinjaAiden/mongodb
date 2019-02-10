a074ed908802f99c3059d4a8557f52a407d4e0f2
Aiden Harvey <aidenharvey102@gmail.com>
2019-02-10 14:38:49 +0000

    added function to return entries in database

--- mongo_project.py
+++ mongo_project.py
@@ -5,12 +5,31 @@ MONGODB_URI = os.getenv("MONGO_URI")
 DBS_NAME = "my-test-db"
 COLLECTION_NAME = "myFirstMDB"
 
-def mongo_connection(url):
+def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
+
+
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+    except:
+        print("Error accessing the database")
+    
+    if not doc:
+        print("")
+        print("Error! No results found.")
+    
+    return doc
+
 
 def show_menu():
     print("")
@@ -19,24 +38,10 @@ def show_menu():
     print("3. Edit a record")
     print("4. Delete a record")
     print("5. Exit")
-    
-    option = input("Enter an option: ")
+
+    option = input("Enter option: ")
     return option
 
-def get_record():
-    print("")
-    first = input("Enter first name > ")
-    last = input("Enter last name > ")
-    
-    try:
-        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
-    except:
-        print ("Error accessing databse")
-    if not doc:
-        print("")
-        print("Error! No results found")
-    
-    return doc
 
 def add_record():
     print("")
@@ -47,10 +52,10 @@ def add_record():
     hair_colour = input("Enter hair colour > ")
     occupation = input("Enter occupation > ")
     nationality = input("Enter nationality > ")
-    
-    new_doc = { 'first': first.lower(), 'last': last.lower(), 'dob': dob,
-                'gender': gender, 'hair_colour': hair_colour, 'occupation': occupation,
-                'nationality': nationality }
+
+    new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob,
+               'gender': gender, 'hair_colour': hair_colour, 'occupation':
+               occupation, 'nationality': nationality}
     
     try:
         coll.insert(new_doc)
@@ -58,18 +63,28 @@ def add_record():
         print("Document inserted")
     except:
         print("Error accessing the database")
-    
+
+
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
             add_record()
         elif option == "2":
-            print("You have selected option 2")
+            find_record()
         elif option == "3":
-            print("You have selected option 3")
+            edit_record()
         elif option == "4":
-            print("You have selected option 4")
+            delete_record()
         elif option == "5":
             conn.close()
             break
@@ -77,7 +92,8 @@ def main_loop():
             print("Invalid option")
         print("")
 
-conn = mongo_connection(MONGODB_URI)
+
+conn = mongo_connect(MONGODB_URI)
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
 main_loop()
